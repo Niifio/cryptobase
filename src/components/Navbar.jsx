3d83e36219e0c5bc9c940a18b0c23ac3ba40f873
niifio <josephniifio@yahoo.com>
2022-03-10 06:54:05 +0400

    new changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import icon from "../images/cryptocurrency.png";
 import {
   HomeOutlined,
-  MoneyCollectOutlined,
   BulbOutlined,
   FundOutlined,
   MenuOutlined,
